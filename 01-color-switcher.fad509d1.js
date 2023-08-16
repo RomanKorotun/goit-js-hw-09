84aaca54752230af6014253dfb673d64d958e6e9
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2023-08-16 11:52:11 +0000

    Deploying to gh-pages from @ RomanKorotun/goit-js-hw-09@becee18833bf637240a0d10d530708072a08e70d 🚀

---
+++ 01-color-switcher.fad509d1.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]");e.addEventListener("click",(function(e){n=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),e.target.disabled=!0,a.disabled=!1}));var n=null;a.addEventListener("click",(function(t){clearInterval(n),t.target.disabled=!0,e.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.fad509d1.js.map
